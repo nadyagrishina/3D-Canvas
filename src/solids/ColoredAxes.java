e439d19629025bf7039a3d38d28fb7920a55c033
Nadezhda <nadyagr@bk.ru>
2023-12-10 14:13:40 +0100

    Initial commit

---
+++ src/solids/ColoredAxes.java
@@ -0,0 +1,25 @@
+package solids;
+
+import transforms.*;
+
+import java.awt.*;
+import renderer.WiredRenderer;
+public class ColoredAxes {
+    private final WiredRenderer wiredRenderer;
+
+    public ColoredAxes(WiredRenderer wiredRenderer) {
+        this.wiredRenderer = wiredRenderer;
+    }
+
+    public void render() {
+        renderAxis(new Vec3D(1, 0, 0), Color.decode("#00FF00"));
+        renderAxis(new Vec3D(0, 1, 0), Color.decode("#FF0033"));
+        renderAxis(new Vec3D(0, 0, 1), Color.decode("#0033FF"));
+    }
+
+    private void renderAxis(Vec3D direction, Color color) {
+        Point3D origin = new Point3D(0, 0, 0);
+        Point3D end = new Point3D(direction);
+        wiredRenderer.renderLine(origin, end, color);
+    }
+}
