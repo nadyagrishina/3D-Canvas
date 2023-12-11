00a690f3c9713468e6e4d198d7651607bf4d704f
Nadezhda <nadyagr@bk.ru>
2023-12-11 01:18:26 +0100

    Initial commit

---
+++ src/solids/Ferguson.java
@@ -0,0 +1,29 @@
+package solids;
+
+import transforms.Cubic;
+import transforms.Mat4;
+import transforms.Point3D;
+
+import java.util.ArrayList;
+public class Ferguson extends Solid {
+    double step;
+    public Ferguson(final int smoothness, Point3D a, Point3D b, Point3D c, Point3D d) {
+        Mat4 fergunson;
+        vb = new ArrayList<>();
+        ib = new ArrayList<>();
+        fergunson = Cubic.FERGUSON;
+
+        step = (Math.PI * 2 / smoothness);
+        Cubic cubic = new Cubic(fergunson, a, b, c, d);
+
+        for (int i = 0; i < smoothness; i++) {
+            vb.add(cubic.compute((double) i / smoothness));
+
+            if (i != 0) {
+                ib.add(i - 1);
+                ib.add(i);
+            }
+        }
+    }
+}
+
