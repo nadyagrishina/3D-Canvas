2fafe112060327f27773ea8f9109cd2400ca0725
Valkryst <github@valkryst.com>
2025-05-05 16:36:41 -0300

    Remove unused methods from `LineRasterizer`

--- src/main/java/com/nadyagrishina/canvas3d/rasterize/LineRasterizer.java
+++ src/main/java/com/nadyagrishina/canvas3d/rasterize/LineRasterizer.java
@@ -1,27 +1,12 @@
 package com.nadyagrishina.canvas3d.rasterize;
 
-import com.nadyagrishina.canvas3d.model.Line;
-
 import java.awt.*;
 
 public class LineRasterizer {
     Raster raster;
-    Color color;
 
     public LineRasterizer(Raster raster){
         this.raster = raster;
-    }
-
-    public void setColor(Color color) {
-        this.color = color;
-    }
-
-    public void setColor(int color) {
-        this.color = new Color(color);
-    }
-
-    public void rasterize(Line line, Color color) {
-        drawLine(line.getX1(), line.getY1(), line.getX2(), line.getY2(), color);
     }
 
     public void rasterize(int x1, int y1, int x2, int y2, Color color) {
