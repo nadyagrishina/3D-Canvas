90235654f0a6a0bcf23d66d9446483f56e22666e
Valkryst <github@valkryst.com>
2025-05-05 16:37:51 -0300

    Update `LineRasterizer` to combine the `rasterize` and `drawLine` methods

--- src/main/java/com/nadyagrishina/canvas3d/rasterize/LineRasterizer.java
+++ src/main/java/com/nadyagrishina/canvas3d/rasterize/LineRasterizer.java
@@ -10,10 +10,6 @@ public class LineRasterizer {
     }
 
     public void rasterize(int x1, int y1, int x2, int y2, Color color) {
-        drawLine(x1, y1, x2, y2, color);
-    }
-
-    protected void drawLine(int x1, int y1, int x2, int y2, Color color) {
         Graphics2D g = (Graphics2D) ((RasterBufferedImage)raster).getImg().getGraphics();
         g.setStroke(new BasicStroke(2));
         g.setColor(color);
