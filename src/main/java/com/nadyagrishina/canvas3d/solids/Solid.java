092e594ea2e8991d8963a3cb885568bf3d44d1a6
Valkryst <github@valkryst.com>
2025-05-06 09:38:05 -0300

    Update `Solid` to use Lombok's `@Getter` and `@Setter` annotations

--- src/main/java/com/nadyagrishina/canvas3d/solids/Solid.java
+++ src/main/java/com/nadyagrishina/canvas3d/solids/Solid.java
@@ -3,33 +3,20 @@ package com.nadyagrishina.canvas3d.solids;
 import com.nadyagrishina.canvas3d.transforms.Mat4;
 import com.nadyagrishina.canvas3d.transforms.Mat4Identity;
 import com.nadyagrishina.canvas3d.transforms.Point3D;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 
+@Getter
 public class Solid {
     protected ArrayList<Point3D> vb = new ArrayList<>();
     protected ArrayList<Integer> ib = new ArrayList<>();
 
-    protected Mat4 model = new Mat4Identity();
-
-    public ArrayList<Point3D> getVb() {
-        return vb;
-    }
-
-    public ArrayList<Integer> getIb() {
-        return ib;
-    }
+    @Setter protected Mat4 model = new Mat4Identity();
 
     protected void addIndices(Integer... indices) {
         ib.addAll(Arrays.asList(indices));
-    }
-
-    public Mat4 getModel() {
-        return model;
-    }
-
-    public void setModel(Mat4 model) {
-        this.model = model;
     }
 }
